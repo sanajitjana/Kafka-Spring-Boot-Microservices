2c2ea4414b5a06503e20cc31948cb002ca666078
Sanajit Jana <sanajit.jana@mplifytech.com>
2025-06-18 14:07:59 +0530

    Initial commit

---
+++ inventory-service/src/main/java/com/sanajitjana/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sanajitjana.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-service/src/test/java/com/sanajitjana/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanajitjana.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
