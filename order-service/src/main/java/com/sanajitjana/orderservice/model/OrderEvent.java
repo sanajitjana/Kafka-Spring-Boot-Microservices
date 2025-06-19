694a58e9920080e03b736658a41b5642749e831f
Sanajit Jana <sanajit.jana@mplifytech.com>
2025-06-19 10:19:14 +0530

    Initial commit

--- order-service/src/main/java/com/sanajitjana/orderservice/model/OrderEvent.java
+++ order-service/src/main/java/com/sanajitjana/orderservice/model/OrderEvent.java
@@ -1,12 +1,8 @@
 package com.sanajitjana.orderservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrderEvent {
     private String orderId;
     private String userId;
